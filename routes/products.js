f5044e29089b0010e737abc768dd8a27de789513
kunxin-chor <chorkunxin@yahoo.com>
2022-12-13 03:13:26 +0000

    Added update and delete

--- routes/products.js
+++ routes/products.js
@@ -53,4 +53,72 @@ router.post('/add', function(req,res){
     })
 })
 
+router.get("/update/:product_id", async function(req,res){
+    // extract product id of the product that we want to updage
+    const productId = req.params.product_id;
+
+    // get the product that we are updating
+    // SELECT * FROM products WHERE id = '${productId}'
+    const product = await Product.where({
+        'id': productId
+    }).fetch({
+        require: true
+    })
+    // .where basically add in the "WHERE ...."
+    // .fetch executes the query
+    // the `require: true` means that if no results are retrieved, Bookshelf
+    // will cause an exception
+
+    const productForm = createProductForm();
+    // product.get allows us to retrieve one column's value
+    productForm.fields.name.value = product.get('name');
+    productForm.fields.cost.value = product.get('cost');
+    productForm.fields.description.value = product.get('description');
+
+    res.render('products/update',{
+        'form': productForm.toHTML(bootstrapField)
+    });
+})
+
+router.post('/update/:product_id', async function(req,res){
+    const productId = req.params.product_id;
+    const product = await Product.where({
+        'id': productId
+    }).fetch({
+        require: true
+    })
+
+    const productForm = createProductForm();
+    // pass the request as the first parameter
+    // second parameter: object that has three handlers
+    productForm.handle(req, {
+        'success': async function(form) {
+            // form has no validation error
+            // then we proceed to update the product
+            // Instead of:
+            // product.set('name', form.data.name);
+            // product.set('cost', form.data.cost);
+            // product.set('description', form.data.description);
+            // if the parameter to product.set is an object
+            // Bookshelf ORM will try to assign each key as a column
+            product.set(form.data);
+            product.save(); // make the change permanent
+            res.redirect('/products');
+        },
+        'empty': async function(form) {
+            // form has no data
+            res.render('products/update',{
+                'form': form
+            })
+        },
+        'error': async function(form) {
+            // one or more fields have validation errors
+            res.render('products/update', {
+                'form': form
+            })
+        }
+    })
+
+})
+
 module.exports = router;
